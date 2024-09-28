8b976fc9bab4c4766572cdd029d93fa70003b606
mohamad6370 <avandmohammad63@gmail.com>
2024-09-28 08:15:37 +0330

    finish this project

--- app/src/main/java/com/example/myeducompose/MainActivity.kt
+++ app/src/main/java/com/example/myeducompose/MainActivity.kt
@@ -1,30 +1,31 @@
 package com.example.myeducompose
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myeducompose.ui.theme.MyEduComposeTheme
@@ -35,61 +36,106 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyEduComposeTheme {
-               SetUi(this )
+                SetUi()
             }
         }
     }
-}
-@Composable
-fun SetUi(context: Context?){
-    Column {
-        Text(
-            text = "mmadmadi",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 50.dp)
-                .background(color = Color.LightGray)
-                .padding(30.dp),
+        private val username = mutableStateOf(TextFieldValue())
+        private val password = mutableStateOf(TextFieldValue())
+        private val phone = mutableStateOf(TextFieldValue())
 
+        private val userError = mutableStateOf(false)
+        private val passError = mutableStateOf(false)
+        private val phoneError = mutableStateOf(false)
 
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.Bold,
-            fontSize = 28.sp,
-            textAlign = TextAlign.Center,
-            color = colorResource(id = R.color.teal_700),
-        )
-        Button(onClick = {
-            Toast.makeText(
-                context,
-                "test app",
-                Toast.LENGTH_SHORT
-            ).show()
-        },
-            modifier = Modifier
-                .width(200.dp)
-                .padding(start = 70.dp),
-            shape = RoundedCornerShape(10.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color. Blue
-            )
+        @Composable
+        fun SetUi() {
+            Column(
+                Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_profile), contentDescription = null,
+                    modifier = Modifier
+                        .padding(30.dp)
+                        .width(200.dp)
+                        .height(200.dp),
+                )
+
+                MyEditText("نام کاربری", username, userError, false)
+                MyEditText("رمز عبور", password, passError, false)
+                MyEditText("شماره تلفن", phone, phoneError, true)
+
+                Button(
+                    onClick =
+                    {
+                        userError.value =username.value.text.isEmpty()
+                        passError.value =password.value.text.length>5
+                        phoneError.value =phone.value.text.length>11
+                    },
+                    modifier = Modifier
+                        .width(280.dp)
+                        .padding(top = 20.dp)
+                ) {
+                    Text(
+                        text = "Click Here",
+                        fontSize = 22.sp
+                    )
+                }
+
+            }
+
+        }
+
+        @Composable
+        fun MyEditText(
+            label: String,
+            valueText: MutableState<TextFieldValue>,
+            valueError: MutableState<Boolean>,
+            isNumber: Boolean
         ) {
-            Text(
-                text = "Click",
-                color = Color.White
+            var text by remember {
+                valueText
+            }
+            val error by remember {
+                valueError
+            }
+
+            valueError.value = false
+
+            TextField(value = text,
+                onValueChange = {
+                    text = it
+                },
+                label = {
+                    Text(
+                        text = label
+                    )
+
+                },
+                keyboardOptions = if (isNumber)
+                    KeyboardOptions(keyboardType = KeyboardType.Number)
+                else
+                    KeyboardOptions(keyboardType = KeyboardType.Text),
+                textStyle = TextStyle(
+                    fontSize = 20.sp
+                ),
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .width(280.dp),
+                isError = error,
+                supportingText = {
+                    if (error)
+                        Text(text = "لطفا فیلد خالی را پر کنید")
+                }
+
             )
+
         }
     }
-}
 
 
 
 
-@Preview(showBackground = true,
-    showSystemUi = true,
-    device = Devices.PIXEL_6_PRO)
-@Composable
-fun GreetingPreview() {
-    MyEduComposeTheme {
-        SetUi(null)
-    }
-}
+
+
